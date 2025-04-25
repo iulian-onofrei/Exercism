aff109bc5567df269d8c2f2e14f1132b4662fb79
iulian-onofrei1 <iulian.onofrei@mit-dev.com>
2025-04-26 01:31:14 +0300

    interest is interesting

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,23 @@
 (ns interest-is-interesting)
 
 (defn interest-rate
-  "Returns the interest rate based on the specified balance."
   [balance]
-  )
+  (cond 
+    (< balance 0) -3.213
+    (< balance 1000) 0.5
+    (< balance 5000) 1.621
+    :else 2.475))
 
 (defn annual-balance-update
-  "Returns the annual balance update, taking into account the interest rate."
   [balance]
-  )
+    (+ balance 
+      (* 
+        1/100
+        balance
+        (bigdec (Math/abs (interest-rate balance))))))
 
 (defn amount-to-donate
-  "Returns how much money to donate based on the balance and the tax-free percentage."
   [balance tax-free-percentage]
-  )
+    (if (not (neg? balance))
+      (int (* 2 balance (/ tax-free-percentage 100))) 
+      0))
